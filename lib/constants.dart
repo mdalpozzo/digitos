5e57fbf4f5b89cbb04fdb6bec89b5032ff3a124a
marlin dalpozzo <marlin.dalpozzo@gmail.com>
2023-12-31 14:29:41 -0800

    copy in original constants file.

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+class FirestorePaths {
+  static const String DAILY_PUZZLE_COLLECTION = 'daily_puzzles';
+  static const String PUZZLE_COLLECTION = 'puzzles';
+
+  static const String DAILY_PUZZLE_ID_FIELD = 'puzzle_id';
+}
